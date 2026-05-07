a2876c2a138ac752255e4341f4d003162e6768ad
mangojeong <wkdgnl123@cau.ac.kr>
2026-05-07 16:08:22 +0900

    fix: configure github pages routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Landing from '../views/Landing.vue';
 import Dashboard from '../views/Dashboard.vue';
 import Records from '../views/Records.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'landing', component: Landing },
     { path: '/dashboard', name: 'dashboard', component: Dashboard },
     { path: '/records', name: 'records', component: Records },
-    { path: '/sports/:sportKey', name: 'sport-records', component: Records, props: true }
-  ]
+    {
+      path: '/sports/:sportKey',
+      name: 'sport-records',
+      component: Records,
+      props: true,
+    },
+  ],
 });
 
 export default router;
